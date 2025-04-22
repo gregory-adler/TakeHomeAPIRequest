6cde26e95e8a77fb96357e24e76cd4fdce6bb2f0
greg-code-5500 <apitestpage55@gmail.com>
2025-04-22 17:46:05 -0400

    Code Cleanup2

--- requests.js
+++ requests.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const axios = require('axios');
 
+//Auth
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
 const credentials = Buffer.from(`${API_KEY}:${API_SECRET}`).toString('base64');
